a3f34d42eb1e21ac1d509c585019f04bcc169163
esnya <esnya@nekomter.com>
2020-07-27 05:27:35 +0900

    :building_construction: Add BRANCH arg into autopatch scripts

--- scripts/autopatch_rebase.sh
+++ scripts/autopatch_rebase.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+BRANCH="${1:-master}"
 
 git submodule update -f
 cd BCDice
 git checkout -b bcdice-js-auto-patch
 patch -p1 < ../patch.diff
 git commit -am "bcdice-js-auto-patch"
-git checkout master
+git checkout $BRANCH
 git pull --rebase
 git checkout bcdice-js-auto-patch
-git rebase master
+git rebase $BRANCH
